6b622c959d7cb407c27c7beea8b13c617fab5f6a
bibhutighimire <ghimirebibhuti@gmail.com>
2020-08-04 02:19:50 -0600

    fooddetails page has back to mygoodrecipes button added which works

--- src/Component/FoodDetails.js
+++ src/Component/FoodDetails.js
@@ -1,15 +1,14 @@
 import React from "react";
-//import "../style/MyGoodRecipe.css";
-
-
+import "../style/FoodDetails.css";
 class FoodDetails extends React.Component {
-  
+    goBack() {
+        this.props.history.push("/MyGoodRecipes");
+      }
 render() 
 { 
-        return (
-            
+        return (            
             <div>
-               
+            <button onClick= {this.goBack.bind(this)}>Click Here</button>
             <h1>Welcome to Food Details Page</h1>
             </div>
         );
