d9b542a800ae17ec69bed60c6e76c0ebd5912bd0
CorinnaK <corinna.kelly@outlook.com>
2020-08-04 16:06:48 -0600

    background size and text size more appropriate

--- src/Component/AboutUs.js
+++ src/Component/AboutUs.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import social from "../images/001-github-sign.png";
+import MainHeader from "./MainHeader";
 
 export default class AboutUs extends Component {
   constructor() {
@@ -48,6 +49,7 @@ export default class AboutUs extends Component {
     const { authors } = this.state;
     return (
       <>
+        <MainHeader titleHeader="About Us" />
         {authors.map((author) => (
           <section>
             <h2>{author.title}</h2>
