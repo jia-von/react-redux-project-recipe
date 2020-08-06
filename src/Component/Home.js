109ec3eb78a28fafbc2f5634838a1263e564a01c
CorinnaK <corinna.kelly@outlook.com>
2020-08-05 23:19:40 -0600

    fixed export error on home

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -4,19 +4,16 @@ import MainHeader from "./MainHeader";
 import RecGallery from "./RecGallery";
 import React, { Component } from "react";
 
-
-const Home = () => {
-  render();
-  {
+export default class Home extends Component {
+  render() {
     return (
       <>
         <MainHeader titleHeader="Good Girls Eat" />
 
-        <RecGallery recGallery="RecGallery"/>
+        <RecGallery recGallery="RecGallery" />
 
         <Footer />
       </>
-
     );
   }
 }
