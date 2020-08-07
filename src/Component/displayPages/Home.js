7e3337df4d9b458cd25956d458a5751f0b48804e
Jia <jiavon@ualberta.ca>
2020-08-06 22:30:06 -0600

    home page completed without the required background image as shown in the mock up

--- src/Component/displayPages/Home.js
+++ src/Component/displayPages/Home.js
@@ -1,17 +1,35 @@
-import "../../style/App.css";
-import Footer from "../logic/Footer";
 import MainHeader from "../logic/MainHeader";
-import RecGallery from "./RecGallery";
-import React, { Component } from "react";
+import "../../style/Home.css"
+import {connect} from "react-redux";
+import React from "react";
+import Footer from "../logic/Footer";
 
-export default class Home extends Component {
+class Home extends React.Component {
   render() {
     return (
-      <>
-        <RecGallery recGallery="RecGallery" />
-
-        <Footer />
-      </>
-    );
-  }
-}
+      <div className="recGalleryContainer">
+      <MainHeader titleHeader="Good Girls Eat" className="recGalleryHeader"/>
+        <h2>Are you ready to cook?</h2> 
+          {/*Start of the Recipe Gallery for the landing page*/}
+          {this.props.recipe.map((recipe) => 
+            recipe.map((indrecipe) => (
+          
+              <figure className="recGalleryFigure">
+                  <img src={indrecipe.image}></img>
+                  <ul>
+                  <li>
+                      {indrecipe.title} 
+                    </li>
+                    <li>
+                      {indrecipe.summary} 
+                    </li>
+                  </ul>
+              </figure>
+          )))}
+          {/*End of the Recipe Gallery for the landing page*/}
+          <Footer />
+      </div>
+          );
+          }
+        }
+export default connect((state) => {return {recipe: state};})(Home);
