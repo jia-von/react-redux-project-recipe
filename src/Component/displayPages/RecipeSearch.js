aebe34cd7d9f6be18d0e213669b8a690267c66ed
Aaron <aaron.m.champagne@gmail.com>
2020-08-08 16:16:33 -0600

    Aaron wrote this: fixed search bug by elevating the results array and assignment of values to the results array out of the render method and into the search method

--- src/Component/displayPages/RecipeSearch.js
+++ src/Component/displayPages/RecipeSearch.js
@@ -6,7 +6,11 @@ import { connect } from "react-redux";
 import { faHeart, faHamburger } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { toggleFavorites } from "../../actions/recipeAction";
-
+/*
+CITATION: Aaron and Corinna debugged a search issue together. 
+Aaron ended up writing 5 lines of code to fix one bug and make a suggestion about another bug.
+Also I am Aaron. I wrote this citation. 
+*/
 //   * solid heart  <i class="fas fa-heart"></i>
 //   * open heart <i class="far fa-heart"></i>
 
@@ -23,13 +27,12 @@ class RecipeSearch extends React.Component {
     event.preventDefault();
     //console.log(this.state);
     // Create an empty Array to hold the recipes that get returned as having the search term in thier title
-    let newArray = [];
-
+    let newArray = this.props.recipe > 0 ? this.props.recipe : this.state.recipeList;
     const userInput = event.target.value;
 
     // Search through the store with the results from the API searching for titles with the searched text
     // Push the entire recipe to the Array if it matches
-    {
+    if(newArray.length === 0){
       this.props.recipe.map((recipe) =>
         recipe.map((indrecipe) =>
           indrecipe.title
@@ -53,14 +56,9 @@ class RecipeSearch extends React.Component {
   }
 
   render() {
-    // if recipeList state is empty use this.state.recipeList else use this.props.recipe
-
-    let recipes =
-      this.state.recipeList > 0 ? this.state.recipeList : this.props.recipe;
-
     return (
       <div>
-        <>{console.log(recipes)}</>
+        {/* <>{console.log(recipes)}</> */}
         <> {console.log(this.state.recipeList)}</>
         <>{console.log(this.props.recipe)} </>
 
@@ -80,6 +78,8 @@ class RecipeSearch extends React.Component {
             onChange={(event) =>
               this.updateItem("userSearch", event.target.value)
             }
+            // after you finish null checking your dirty data you can just run this search on submit AND change
+            // onChange = {this.search}
           />
           <input type="submit" id="submit" />
         </form>
@@ -88,7 +88,7 @@ class RecipeSearch extends React.Component {
     Render of the results - or on default the entire list of recipes
  */}
 
-        {recipes.map((recipe) =>
+        {this.state.recipeList.map((recipe) =>
           recipe.map((indrecipe) => (
             <>
               <h2>{indrecipe.title}</h2>
@@ -107,7 +107,7 @@ class RecipeSearch extends React.Component {
                 <figcaption>
                   <ul>
                     <li className="label">
-                      {indrecipe.summary.replace(/(<([^>]+)>)/gi, "")}
+                      {indrecipe.summary ? indrecipe.summary.replace(/(<([^>]+)>)/gi, ""):''}
                     </li>
                     <li>
                       <span className="topicHeader">Ingredients: </span>
